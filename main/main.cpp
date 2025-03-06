725dd3f3fdd18c0360db66f7b5dbb53a992ba45a
William Emfinger <waemfinger@gmail.com>
2025-03-06 16:44:05 -0600

    stubbing out esp now sending

--- main/main.cpp
+++ main/main.cpp
@@ -8,9 +8,10 @@
 
 #include "esp_mac.h"
 
-#include "espnow_utils.h"
 #include "espnow.h"
 #include "espnow_ctrl.h"
+#include "espnow_storage.h"
+#include "espnow_utils.h"
 
 #include "esp-box.hpp"
 #include "kalman_filter.hpp"
@@ -19,6 +20,21 @@
 #include "vector2d.hpp"
 
 using namespace std::chrono_literals;
+
+extern "C" esp_err_t on_esp_now_recv(uint8_t *src_addr, void *data,
+                                            size_t size, wifi_pkt_rx_ctrl_t *rx_ctrl) {
+  // ESP_PARAM_CHECK(src_addr);
+  // ESP_PARAM_CHECK(data);
+  // ESP_PARAM_CHECK(size);
+  // ESP_PARAM_CHECK(rx_ctrl);
+
+  // static uint32_t count = 0;
+
+  // ESP_LOGI(TAG, "espnow_recv, <%" PRIu32 "> [" MACSTR "][%d][%d][%u]: %.*s",
+  //          count++, MAC2STR(src_addr), rx_ctrl->channel, rx_ctrl->rssi, size, size, (char *)data);
+
+  return ESP_OK;
+}
 
 extern "C" void app_main(void) {
   espp::Logger logger({.tag = "Motor Gesture Control", .level = espp::Logger::Verbosity::DEBUG});
@@ -76,6 +92,24 @@ extern "C" void app_main(void) {
   std::mutex gravity_mutex;
   std::array<float, 3> gravity = {0, 0, 0};
 
+  // initialize the wifi and esp-now stacks
+  espnow_storage_init();
+
+  esp_event_loop_create_default();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  espnow_config_t espnow_config = ESPNOW_INIT_CONFIG_DEFAULT();
+  espnow_init(&espnow_config);
+
+  espnow_set_config_for_data_type(ESPNOW_DATA_TYPE_DATA, true, on_esp_now_recv);
+
   // make a task to read out the IMU data and print it to console
   using namespace std::chrono_literals;
   espp::Timer imu_timer({.period = 10ms,
@@ -126,6 +160,34 @@ extern "C" void app_main(void) {
                              .priority = 10,
                              .core_id = 0,
                          }});
+
+  // make a task to send the command (based on gravity vector) to the motor using esp-now
+  espp::Timer motor_timer({.period = 10ms,
+                           .callback = [&gravity, &gravity_mutex]() -> bool {
+                             std::array<float, 3> g_vector;
+                             {
+                               std::lock_guard<std::mutex> lock(gravity_mutex);
+                                g_vector = gravity;
+                             }
+
+                             espnow_frame_head_t frame_head;
+                             memset(&frame_head, 0, sizeof(espnow_frame_head_t));
+                             frame_head.retransmit_count = CONFIG_RETRY_NUM;
+                             frame_head.broadcast = true;
+
+                             static uint8_t data[10] = {0};
+                             size_t size = 10;
+                             espnow_send(ESPNOW_DATA_TYPE_DATA, ESPNOW_ADDR_BROADCAST, data, size, &frame_head, portMAX_DELAY);
+
+                             return false;
+                           },
+                           .auto_start = true,
+                           .task_config = {
+                               .name = "Motor",
+                               .stack_size_bytes = 6 * 1024,
+                               .priority = 10,
+                               .core_id = 0,
+                           }});
 
   while (true) {
     std::this_thread::sleep_for(1s);
